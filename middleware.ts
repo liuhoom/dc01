cbc3574d32f94953a025cfa03d91ead9356fae4c
pb.liu <liuhoom@gmail.com>
2025-10-13 10:11:28 +0000

    chore：添加publicroute 判断

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,14 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { auth, clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-export default clerkMiddleware()
+const isPublickRoute = createRouteMatcher(['/sign-in', '/sign-up'])
+
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublickRoute(req)) {
+    await auth.protect()
+  }
+})
 
 export const config = {
-  publicRoutes: [],
-
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
